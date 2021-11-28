6d778c64aab54f73937bd99fe8d7a0bfc207d41c
andres-unadista <affernandez999@gmail.com>
2021-11-28 12:02:19 -0500

    add routes product

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import config from './config';
+import productsRoutes from './routes/product.routes';
 
 const app = express();
 
 app.set('port', config.port || 2000)
+app.use(productsRoutes)
 
 export default app
